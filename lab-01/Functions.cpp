fa0cc6b8effe453ea63540399a0f0d5c0cfdf12f
Balázs Ákos <balazs.akos@student.ms.sapientia.ro>
2024-02-22 15:56:21 +0200

    lab1

---
+++ lab-01/Functions.cpp
@@ -0,0 +1,78 @@
+//
+// Created by balaz on 2024. 02. 22..
+//
+
+#include "Functions.h"
+
+int lnko(int a, int b) {
+    while(b != 0){
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int lkkt(int a, int b) {
+    if(a == 0 || b == 0){
+        return 0;
+    }else{
+        return (a * b) / lnko(a, b);
+    }
+}
+
+int power(int b, int e) {
+    if(b < 0){
+        cout << "A szam negativ." << endl;
+        return -1;
+    }else if(e == 0){
+        cout << "Minden szam a 0-on 1!" << endl;
+        return 1;
+    }else {
+        int result = 1;
+        for (int i = 0; i < e; ++i) {
+            result *= b;
+        }
+        return result;
+    }
+}
+
+void read_array(int *a, int n) {
+    cout << "Adj meg " << n << " db szamot: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+}
+
+void print_array(int *a, int n) {
+    cout << "A tomb elemei: ";
+    for (int i = 0; i < n; ++i) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+int linearSearch(int *array, int size, int target) {
+    for (int i = 0; i < size; ++i) {
+        if(array[i] == target){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int binarySearch(const int *array, int size, int target) {
+    int left = 0;
+    int right = size - 1;
+    while(left <= right){
+        int mid = left + (right - left) / 2;
+        if(array[mid] == target){
+            return mid;
+        }else if(array[mid] < target){
+            left = mid + 1;
+        }else{
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
--- lab-01/main.cpp
+++ lab-01/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include "Functions.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "LNKO = " << lnko(1372, 198) << endl;
+    cout << "LKKT = " << lkkt(1372, 198) << endl;
+    cout << "POWER = " << power(2, 5) << endl;
+
+    int n;
+    cout << "Add meg a tomb meretet: ";
+    cin >> n;
+    int *arr = new int[n];
+    read_array(arr, n);
+    print_array(arr, n);
+
+    int target;
+    cout << "Add meg melyik szamot keresed a tombben: ";
+    cin >> target;
+    int result = linearSearch(arr, n, target);
+    if(result != -1){
+        cout << "A keresett szam megtalalva a " << result << " indexen" << endl;
+    }else{
+        cout << "Nincs ilyen szam a tombben." << endl;
+    }
+
+    int result2 = binarySearch(arr, n, target);
+    if(result2 != -1){
+        cout << "A keresett szam megtalalva a " << result << " indexen" << endl;
+    }else{
+        cout << "Nincs ilyen szam a tombben." << endl;
+    }
+
     return 0;
 }
